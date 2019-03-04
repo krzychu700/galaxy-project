f4f55461cd8524b1fc1f1f6ede7c42c0143cac41
Krzysztof Szypulski <krzychu700@gmail.com>
2019-03-04 22:05:31 +0100

    added shadow to plannet, moon and moon orbit

--- galaxy/src/App.js
+++ galaxy/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="galaxy">
         <div className="orbit">
-          <div className="planet" />
+          <div className="pos">
+            <div className="planet">
+              <div className="moonOrbit">
+                <div className="moonPos">
+                  <div className="moon" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
